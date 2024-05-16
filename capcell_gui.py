23fd657b7050219d747dea1dc3b5a005f998604c
katie-broun <katie.broun@duke.edu>
2024-05-16 16:48:08 -0400

    Updates to buttons on GUI for additional work

--- capcell_gui.py
+++ capcell_gui.py
@@ -11,10 +11,23 @@ def main():
     title_label = ttk.Label(root, text='Capcell Gen 3 Imaging') #creates label 
     title_label.grid(column=0, row=0)
     sample_label = ttk.Label(root, text='Image/Sample Name:')
-    sample_label.grid(column=0, row=1, sticky=tk.W) # stick it to the east of the cell
+    sample_label.grid(column=0, row=1, sticky=tk.W) # stick it to the west of the cell
     sample_entry = ttk.Entry(root)
     sample_data = tk.StringVar() # will need this in saving function later
     sample_entry.grid(column=1, row=1)
+    live_view_btn = ttk.Button(root, text='Live')
+    live_view_btn.grid(column=2, row=1)
+    capture_btn = ttk.Button(root, text='Capture')
+    capture_btn.grid(column=2, row=2)
+    load_setting_label = ttk.Label(root, text='Load Settings')
+    load_setting_label.grid(column=0, row=3)
+    bodipy_btn = ttk.Button(root, text='Bodipy')
+    bodipy_btn.grid(column=0, row=4)
+    tmre_btn = ttk.Button(root, text='TMRE')
+    tmre_btn.grid(column=1, row=4)
+    nbdg_btn = ttk.Button(root, text='2NBDG')
+    nbdg_btn.grid(column=2, row=4)
+    
 
 
 
