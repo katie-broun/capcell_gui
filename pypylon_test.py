f6f7cbac9bcf532df3286a68c3971bfb412f3e07
katie-broun <katie.broun@duke.edu>
2024-05-30 10:31:43 -0400

    Creating test files for camera integration

---
+++ pypylon_test.py
@@ -0,0 +1,30 @@
+from pypylon import pylon
+
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+camera.Open()
+
+# demonstrate some feature access
+new_width = camera.Width.Value - camera.Width.Inc
+if new_width >= camera.Width.Min:
+    camera.Width.Value = new_width
+
+numberOfImagesToGrab = 100
+camera.StartGrabbingMax(numberOfImagesToGrab)
+
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grabResult.GrabSucceeded():
+        # Access the image data.
+        print("SizeX: ", grabResult.Width)
+        print("SizeY: ", grabResult.Height)
+        img = grabResult.Array
+        print("Gray value of first pixel: ", img[0, 0])
+
+    grabResult.Release()
+camera.Close()
+
+# As of 5/28 with connected extra camera,
+# we are able to see what the camera sees in python
+
+# we are working to determine what makes the most sense from a size and image array
